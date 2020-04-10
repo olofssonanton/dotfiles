8c71abf486db2cfd7101918e10d32cc9b032a575
Anton Olofsson <me@antonolofsson.com>
2020-04-10 12:45:06 +0200

    Add more dotfiles, along with setup script

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Stolen from https://github.com/tomnomnom/dotfiles/
+
+dotfilesDir=$(pwd)
+
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${1} ]; then
+    # Existing symlink
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest}
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+linkDotfile .zshrc
+linkDotfile .gitconfig
+linkDotfile .tmux.conf
+linkDotfile .xinitrc
